f6a2fdbd7bf096e4821534a642c71650d624a305
Kim <72054684+kimcascante@users.noreply.github.com>
2026-03-28 10:18:32 -0600

    Revert "Jared feature dev"

--- PSA.EntidadesDTO/DTOs/Usuarios/RolDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PSA.EntidadesDTO.DTOs.Usuarios
-{
-    public class RolDTO
-    {
-        public int Id { get; set; }
-        public string Nombre { get; set; } = string.Empty;
-        public string? Descripcion { get; set; }
-    }
-}
