6886334c1e6bb15c28ed6cfd3239a29a5e31c798
dcruzm-ops <dcruzm@ucenfotec.ac.cr>
2026-03-30 12:46:57 -0600

    fix: send welcome email from WebApp registration flows

--- PSA.EntidadesDTO/DTOs/RecuperacionContrasena/RestablecerContrasenaDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PSA.EntidadesDTO.DTOs
-{
-    public class RestablecerContrasenaDTO
-    {
-        public string Token { get; set; } = string.Empty;
-        public string NuevaContrasena { get; set; } = string.Empty;
-        public string ConfirmarContrasena { get; set; } = string.Empty;
-    }
-}
